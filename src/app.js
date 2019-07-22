ddc53c1dc5d384aa41b15a6ef353b1d8cad25c0e
Ivan Eisenberg <ivaneisenberg@northwesternmutual.com>
2019-07-22 09:57:13 -0500

    add start script

--- src/app.js
+++ src/app.js
@@ -7,4 +7,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
+  console.log(`Presentation: http://localhost:${port}`)
+})
